7a2356ae6daf29056f836e4b6c3eb959d161fefa
Tsurosaki <artomdakov52@gmail.com>
2025-03-01 11:01:25 +1000

    lab2.c
    Lab_2

--- lab2.c
+++ lab2.c
@@ -2,10 +2,10 @@
 int main() {
     double matrix[3][3];
     double main_diagonal_sum = 0.0, secondary_diagonal_sum = 0.0;
-    printf("Ââåäèòå çíà÷åíèÿ äëÿ ìàòðèöû 3x3:\n");
+    printf("Введите значения для матрицы 3x3:\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            printf("Ýëåìåíò [%d][%d]: ", i + 1, j + 1);
+            printf("Элемент [%d][%d]: ", i + 1, j + 1);
             scanf("%lf", &matrix[i][j]);
         }
     }
@@ -15,7 +15,7 @@ int main() {
     for (int i = 0; i < 3; i++) {
         secondary_diagonal_sum += matrix[i][2 - i];
     }
-    printf("Ñóììà ýëåìåíòîâ íà ãëàâíîé äèàãîíàëè: %.2lf\n", main_diagonal_sum);
-    printf("Ñóììà ýëåìåíòîâ íà ïîáî÷íîé äèàãîíàëè: %.2lf\n", secondary_diagonal_sum);
+    printf("Сумма элементов на главной диагонали: %.2lf\n", main_diagonal_sum);
+    printf("Сумма элементов на побочной диагонали: %.2lf\n", secondary_diagonal_sum);
     return 0;
 }
