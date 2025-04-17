34c40d75433e46814039fd0b3648da215158d5e4
Tsurosaki <artomdakov52@gmail.com>
2025-04-17 23:24:25 +1000

    triangle.c

--- my_project/triangle.c
+++ my_project/triangle.c
@@ -6,7 +6,7 @@ double calculate_perimeter(double a, double b, double c) {
 }
 // Функция для вычисления площади треугольника по формуле Герона
 double calculate_area(double a, double b, double c) {
-    double s = (a + b + c) / 2; // Полупериметр
+    double s = (a + b + c) / 2;
     return sqrt(s * (s - a) * (s - b) * (s - c));
 }
 double calculate_height(double area, double site) {
