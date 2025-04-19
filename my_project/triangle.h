3468ac7e698b1fadc25f97811ba910c0c7b1f265
Tsurosaki <artomdakov52@gmail.com>
2025-04-19 10:53:23 +1000

    triangle.h

--- my_project/triangle.h
+++ my_project/triangle.h
@@ -1,6 +1,7 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
+int is_valid_triangle(double a, double b, double c);
 double calculate_perimeter(double a, double b, double c);
 double calculate_area(double a, double b, double c);
 double calculate_height(double area, double site);
