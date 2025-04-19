8e9301c68806ec0afb93dc83310b2bbcd10d1436
Tsurosaki <artomdakov52@gmail.com>
2025-04-19 11:06:30 +1000

    main.c

--- my_project/main.c
+++ my_project/main.c
@@ -10,10 +10,9 @@ int main() {
     if (is_valid_triangle(a, b, c)) {
         double perimeter = calculate_perimeter(a, b, c);
         double area = calculate_area(a, b, c);
+        double height = calculate_height(area, b);
         printf("Периметр треугольника: %.2lf\n", perimeter);
         printf("Площадь треугольника: %.2lf\n", area);
-    
-        double height = calculate_height(area, b);
         printf("Высота треугольника по выбранной стороне %.2lf: %.2lf\n", b, height);
     } 
     else {
