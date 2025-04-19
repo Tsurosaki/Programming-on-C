4f20fab80d01b508fa64c051045cdb0bcad87982
Tsurosaki <artomdakov52@gmail.com>
2025-04-19 10:52:43 +1000

    triangle.c

--- my_project/triangle.c
+++ my_project/triangle.c
@@ -1,9 +1,13 @@
 #include <math.h>
 
+int is_valid_triangle(double a, double b, double c) {
+    return (a + b > c) && (a + c > b) && (b + c > a);
+}
+
 double calculate_perimeter(double a, double b, double c) {
     return a + b + c;
 }
-// Функция для вычисления площади треугольника по формуле Герона
+
 double calculate_area(double a, double b, double c) {
     double s = (a + b + c) / 2;
     return sqrt(s * (s - a) * (s - b) * (s - c));
