dccc70a3b16e0f5d15e37b428e1c80fd0533428e
Tsurosaki <artomdakov52@gmail.com>
2025-04-19 10:52:17 +1000

    main.c

--- my_project/main.c
+++ my_project/main.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
 #include "triangle.h"
-// Функция для проверки, является ли тройка чисел длинами сторон треугольника
-int is_valid_triangle(double a, double b, double c) {
-    return (a + b > c) && (a + c > b) && (b + c > a);
-}
+
 int main() {
     double a, b, c;
 
-    printf("Длина первой стороны треугольника: ");
-    scanf("%lf", &a);
-    printf("Длина второй стороны треугольника: ");
-    scanf("%lf", &b);
-    printf("Длина третьей стороны треугольника: ");
-    scanf("%lf", &c);
+    printf("Длины сторон треугольника: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
 
     if (is_valid_triangle(a, b, c)) {
         double perimeter = calculate_perimeter(a, b, c);
         double area = calculate_area(a, b, c);
         printf("Периметр треугольника: %.2lf\n", perimeter);
         printf("Площадь треугольника: %.2lf\n", area);
-        double site;
-        printf("Длина основания для вычисления высоты: ");
-        scanf("%lf", &site);
     
-        double height = calculate_height(area, a);
-        printf("Высота треугольника по основанию %.2lf: %.2lf\n", a, height);
+        double height = calculate_height(area, b);
+        printf("Высота треугольника по выбранной стороне %.2lf: %.2lf\n", b, height);
     } 
     else {
         printf("Введенные длины не могут образовать треугольник.\n");
