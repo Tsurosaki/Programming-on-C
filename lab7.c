6d9cca4fe1abc8bad6cf629248e3dd7a440c3384
Tsurosaki <artomdakov52@gmail.com>
2025-04-26 10:30:42 +1000

    lab7.c

--- lab7.c
+++ lab7.c
@@ -4,14 +4,12 @@
 #define MAX_NAME_LENGTH 50
 #define ARRAY_SIZE 4
 
-// Объявление структуры humen
 typedef struct {
     char firstName[MAX_NAME_LENGTH];
     char lastName[MAX_NAME_LENGTH];
     int birthYear;
 } humen;
 
-// Функция для сортировки массива по годам рождения
 void sortByBirthYear(humen arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
@@ -24,10 +22,9 @@ void sortByBirthYear(humen arr[], int size) {
     }
 }
 
-// Функция для вывода массива
 void printArray(humen arr[], int size) {
     for (int i = 0; i < size; i++) {
-        printf("Имя: %s, Фамилия: %s, Год рождения: %d\n", arr[i].firstName, arr[i].lastName, arr[i].birthYear);
+        printf("РРјСЏ: %s, Р¤Р°РјРёР»РёСЏ: %s, Р“РѕРґ СЂРѕР¶РґРµРЅРёСЏ: %d\n", arr[i].firstName, arr[i].lastName, arr[i].birthYear);
     }
 }
 
@@ -35,28 +32,24 @@ int main() {
     humen array1[ARRAY_SIZE];
     humen array2[ARRAY_SIZE];
 
-    // Ввод данных в первый массив
-    printf("Введите данные для %d человек:\n", ARRAY_SIZE);
+    printf("Р’РІРµРґРёС‚Рµ РґР°РЅРЅС‹Рµ РґР»СЏ %d С‡РµР»РѕРІРµРє:\n", ARRAY_SIZE);
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("Человек %d:\n", i + 1);
-        printf("Имя: ");
+        printf("Р§РµР»РѕРІРµРє %d:\n", i + 1);
+        printf("РРјСЏ: ");
         scanf("%s", array1[i].firstName);
-        printf("Фамилия: ");
+        printf("Р¤Р°РјРёР»РёСЏ: ");
         scanf("%s", array1[i].lastName);
-        printf("Год рождения: ");
+        printf("Р“РѕРґ СЂРѕР¶РґРµРЅРёСЏ: ");
         scanf("%d", &array1[i].birthYear);
     }
 
-    // Копирование данных во второй массив
     for (int i = 0; i < ARRAY_SIZE; i++) {
         array2[i] = array1[i];
     }
 
-    // Сортировка второго массива по годам рождения
     sortByBirthYear(array2, ARRAY_SIZE);
 
-    // Вывод отсортированного массива
-    printf("\nОтсортированный список по годам рождения:\n");
+    printf("\nРћС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Р№ СЃРїРёСЃРѕРє РїРѕ РіРѕРґР°Рј СЂРѕР¶РґРµРЅРёСЏ:\n");
     printArray(array2, ARRAY_SIZE);
 
     return 0;
